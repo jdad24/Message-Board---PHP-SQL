1cf538d85deaab21ad3377c1872adac6b5dfa05f
jdad24 <49546169+jdad24@users.noreply.github.com>
2019-07-11 21:14:55 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	
+</head>
+<body>
+	<form action="board.php" method="GET">
+		Username: <input type="text" name="username">
+		Password: <input type="password" name="password">
+		<input type="submit" value="submit" name="submit">
+	</form>
+	
+</body>
+</html>
